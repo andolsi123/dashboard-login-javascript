3271185015a2f74b844fcad48f7a6774d65a40ec
andolsi123 <45923768+andolsi123@users.noreply.github.com>
2019-01-20 16:14:34 +0100

    final bug removed(demo app)

--- assets/js/history.js
+++ assets/js/history.js
@@ -4,20 +4,23 @@ var date = new Date();
 for (let i = 0; i < local.length; i++) {
   if (local[i].id === localuser) {
     var tododate = new Date(local[i].dead);
-    if (parseInt(tododate.getFullYear()) <= parseInt(date.getFullYear())) {
-      if (parseInt(tododate.getMonth()) <= parseInt(date.getMonth())) {
-        if (parseInt(tododate.getDay()) <= parseInt(date.getDay())) {
-          var div = document.createElement("div");
-          div.style.minWidth = "100px";
-          div.style.minHeight = "100px";
-          div.style.background = "red";
-          div.style.color = "white";
-          div.innerHTML =
-            "to do: " + local[i].do + "</br>" + "deadline: " + local[i].dead;
+    if (
+      parseInt(tododate.getFullYear()) < parseInt(date.getFullYear()) ||
+      (parseInt(tododate.getFullYear()) === parseInt(date.getFullYear()) &&
+        parseInt(tododate.getMonth()) < parseInt(date.getMonth())) ||
+      (parseInt(tododate.getFullYear()) === parseInt(date.getFullYear()) &&
+        parseInt(tododate.getMonth()) === parseInt(date.getMonth()) &&
+        parseInt(tododate.getDay()) < parseInt(date.getDay()))
+    ) {
+      var div = document.createElement("div");
+      div.style.minWidth = "100px";
+      div.style.minHeight = "100px";
+      div.style.background = "red";
+      div.style.color = "white";
+      div.innerHTML =
+        "to do: " + local[i].do + "</br>" + "deadline: " + local[i].dead;
 
-          document.getElementById("mainblock").appendChild(div);
-        }
-      }
+      document.getElementById("mainblock").appendChild(div);
     }
   }
 }
